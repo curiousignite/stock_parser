fdcde2f67e8730b3c32c38ea98354c23872aad93
curiousignite <berkeerbagci@outlook.com>
2023-12-21 00:15:15 +0300

    Mango added, url link is now input

--- hnm/parser.py
+++ hnm/parser.py
@@ -1,24 +1,25 @@
 import time
 import requests
-from playsound import playsound
+# from playsound import playsound
+# url = "https://www2.hm.com/hmwebservices/service/product/tr/availability/1134755.json"
+
+url = input("H&M ürün linki: ")
 
 
-def extract_source():
-    url = (
-        "https://www2.hm.com/hmwebservices/service/product/tr/availability/1134755.json"
-    )
+def extract_source(url):
     agent = {"User-Agent": "Mozilla/5.0"}
     try:
         json_output = requests.get(url, headers=agent).json()
-        print(time.ctime(), json_output)
+        print(json_output, time.ctime())
         for i in json_output["availability"]:
+            # TODO : Bedenleri listele ve hardcode' çıkart
             if i != "1134755003006":
-                playsound("../alarm.wav")
+                # playsound("../media/alarm.wav")
                 print("works")
     except:
-        print(time.ctime(), "timeout")
+        print("timeout", time.ctime())
 
 
 while True:
-    extract_source()
+    extract_source(url)
     time.sleep(30)
